494e24e10290866c95397d6fe574909c5f5075a3
Hieu <amhnguyen@myseneca.ca>
2019-09-23 01:41:27 -0400

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {EventComponent} from './event/event.component';
+const routes: Routes = [
+  {path: 'events', component: EventComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
