966cd14a5d7fb4fa1fa3931f46a08f7787079e18
AltiV <vuong.alan.t@gmail.com>
2019-09-30 02:02:16 -0400

    Refinement / clean-up of front page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  faHome = faHome;
+  faEnvelope = faEnvelope;
 
   constructor() { }
 
