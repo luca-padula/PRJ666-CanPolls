ceb89157e1cfccb2bf414be67c53165d301fecb1
YuMing Zhang <yumingzhang94@gmail.com>
2019-10-08 08:50:57 -0400

    Revert "official event trial"
    This reverts commit 6c7b0c1f81aa30b656c83fb11da8ed6a3e71dc22.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,6 @@ import { RegisterComponent } from './register/register.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { CreateEventComponent } from './create-event/create-event.component';
 import { VerifyEmailComponent } from './verify-email/verify-email.component';
-import{ OfficialEventComponent } from './official-event/official-event.component'
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -21,7 +20,6 @@ const routes: Routes = [
   {path: 'createEvent', component: CreateEventComponent},
   {path: 'userProfile', component: UserProfileComponent },
   {path: 'verifyEmail/:id/:token', component: VerifyEmailComponent},
-  {path:'officialevent',component:OfficialEventComponent},
   {path: "", redirectTo:'/home', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
 ];
