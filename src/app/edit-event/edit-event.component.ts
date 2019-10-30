8ab2b2866071b7ffca029b9f975043d3d0741821
Luca Padula <lucamp9843@gmail.com>
2019-10-30 00:24:47 -0400

    - removed PasswordRecovery.ts model as we do not need a table in the database - renamed location.ts model to Location.ts (capital L) for consistency - added updateEventById() and getRegisteredUsers() functions to event service - progress on edit event component

--- src/app/edit-event/edit-event.component.ts
+++ src/app/edit-event/edit-event.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/data/services/auth.service';
+import { EventService } from 'src/data/services/event.service';
+import { Event } from 'src/data/Model/Event';
+import { Location } from 'src/data/Model/Location';
+import { User } from 'src/data/Model/User';
+import { ValidationError } from 'src/data/Model/ValidationError';
 
 @Component({
   selector: 'app-edit-event',
@@ -10,11 +16,53 @@ import { AuthService } from 'src/data/services/auth.service';
 export class EditEventComponent implements OnInit {
 
   private token: any;
+  eventId: number;
+  event: Event;
+  location: Location;
+  registeredUsers: User[];
+  paramSubscription: any;
+  getEventSubscription: any;
+  getLocationSubscription: any;
+  getUsersSubscription: any;
+  validationErrors: ValidationError[];
+  successMessage: boolean;
+  warning: string;
 
-  constructor(private auth: AuthService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private auth: AuthService,
+    private eventService: EventService
+  ) { }
 
   ngOnInit() {
     this.token = this.auth.readToken();
+    this.paramSubscription = this.route.params.subscribe((params) => {
+      this.eventId = params['id'];
+    }, (err) => {
+      console.log('Unable to get route parameters', err);
+    });
+    this.getEventSubscription = this.eventService.getEventById(this.eventId).subscribe((result) => {
+      this.event = result;
+    }, (err) => {
+      console.log('Unable to get event', err);
+    });
+    this.location = new Location;
+    this.getUsersSubscription = this.eventService.getRegisteredUsers(this.eventId).subscribe((results) => {
+      this.registeredUsers = results;
+      console.log(this.registeredUsers);
+    }, (err) => {
+      console.log('Unable to get registered users', err);
+    });
   }
 
+  ngOnDestroy() {
+    if (this.paramSubscription)
+      this.paramSubscription.unsubscribe();
+    if (this.getEventSubscription)
+      this.getEventSubscription.unsubscribe();
+    if (this.getLocationSubscription)
+      this.getLocationSubscription.unsubscribe();
+    if (this.getUsersSubscription)
+      this.getUsersSubscription.unsubscribe();
+  }
 }
--- src/data/services/event.service.ts
+++ src/data/services/event.service.ts
@@ -3,6 +3,8 @@ import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {environment} from 'src/environments/environment';
 import {EventToCreate} from 'src/data/Model/EventToCreate';
+import { Event } from 'src/data/Model/Event';
+import { User } from 'src/data/Model/User';
 
 
 @Injectable({
@@ -14,5 +16,13 @@ export class EventService {
 
   getEventById(id: number) : Observable<Event>{
     return this.http.get<Event>(environment.apiUrl + '/api/event/' + id);
+  }
+
+  updateEventById(id: number, event: Event): Observable<Event> {
+    return this.http.put<Event>(environment.apiUrl + '/api/event/' + id, event);
+  }
+
+  getRegisteredUsers(eventId: number): Observable<User[]> {
+    return this.http.get<User[]>(environment.apiUrl + '/api/event/' + eventId + '/registeredUsers');
   }
 }
