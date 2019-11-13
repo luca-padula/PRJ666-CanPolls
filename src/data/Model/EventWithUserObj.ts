5b52cc5b9e37384fa2b202f8342a37072a566993
Harshit Arora <harora4@myseneca.ca>
2019-11-12 23:09:24 -0500

    eventwithuserobj

---
+++ src/data/Model/EventWithUserObj.ts
@@ -0,0 +1,17 @@
+import { User } from 'src/data/Model/User';
+export class EventWithUserObj
+{
+    event_id: number;
+    event_title: string;
+    event_description: string;
+    date_to: string;
+    date_from: string;
+    time_from: string;
+    time_to: string;
+    attendee_limit: number;
+    isApproved: boolean;
+    UserUserId: string;
+    createdAt: string;
+    updatedAt: string;
+    User: User;
+}
