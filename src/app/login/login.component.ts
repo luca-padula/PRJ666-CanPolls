978b6459dd8302b5af8c2bd5d6e7e9e935c9104c
Hieu <amhnguyen@myseneca.ca>
2019-11-18 02:47:14 -0500

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,10 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/data/services/auth.service';
+import {EventService} from 'src/data/services/event.service';
 import { User } from 'src/data/Model/User';
 import {MatDialog, MatDialogConfig} from '@angular/material';
 import { FeedbackComponent } from '../feedback/feedback.component';
+import {Event} from 'src/data/Model/Event';
 export interface DialogData{
+  rating: number;
   description: String;
 }
 @Component({
@@ -21,11 +24,15 @@ export class LoginComponent implements OnInit {
   public userNotVerified: boolean = false;
   public unverifiedUser: string;
   public rejectionCount: number = 0;
+  attendedEvents: any[];
+  public event: Event;
   description: string;
+  rating:number;
   constructor(
     private auth: AuthService,
     private router: Router,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private eService: EventService
   ) { }
 
   ngOnInit() {
@@ -37,8 +44,15 @@ export class LoginComponent implements OnInit {
     this.auth.login(this.user).subscribe((success) => {
       localStorage.setItem('access_token', success.token);
       this.router.navigate(['/home']);
+      this.eService.getEventsAttendedByUser(+this.user.userId).subscribe(data => {
+        console.log(data);
+        this.attendedEvents = data;
+        if(this.attendedEvents.length > 0){
+          this.event = this.attendedEvents[this.attendedEvents.length-1];
+          this.openDialog();
+        }
+      });
       
-      this.openDialog();
     }, (err) => {
       this.rejectionCount++;
       if (this.rejectionCount >= 5) {
@@ -65,7 +79,7 @@ export class LoginComponent implements OnInit {
 
   openDialog(){
     const dialogRef = this.dialog.open(FeedbackComponent, {
-      data: {description: this.description}
+      data: {description: this.description, rating: this.rating}
     });
     dialogRef.afterClosed().subscribe(result =>{
       console.log('the dialog was closed');
