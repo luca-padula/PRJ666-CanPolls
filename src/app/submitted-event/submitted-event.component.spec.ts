33a1185ff141cc575ab6b991991b45e997f5beb4
Luca Padula <lucamp9843@gmail.com>
2021-04-12 13:57:06 -0400

    Add testing for submitted event.
    Update main test.ts file to only run submitted-event spec

--- src/app/submitted-event/submitted-event.component.spec.ts
+++ src/app/submitted-event/submitted-event.component.spec.ts
@@ -1,14 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from 'src/data/services/auth.service';
 
 import { SubmittedEventComponent } from './submitted-event.component';
 
 describe('SubmittedEventComponent', () => {
   let component: SubmittedEventComponent;
   let fixture: ComponentFixture<SubmittedEventComponent>;
+  let authServiceSpy = jasmine.createSpyObj('AuthService', ['readToken', 'isAuthenticated']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SubmittedEventComponent ]
+      declarations: [ SubmittedEventComponent ],
+      imports: [ FormsModule ],
+      providers: [ { provide: AuthService, useValue: authServiceSpy} ]
     })
     .compileComponents();
   }));
