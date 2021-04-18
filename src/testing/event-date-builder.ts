87aa6acb84e7f5099021abb6ab87a22a8444d1b8
Luca Padula <lucamp9843@gmail.com>
2021-04-17 23:22:26 -0400

    Add testing folder with new helper class
    Add event-date-builder helper class.

---
+++ src/testing/event-date-builder.ts
@@ -0,0 +1,20 @@
+export class EventDateBuilder {
+
+    constructor() { }
+    
+    // Helper function to generate event date, start time, and end time.
+    // Parameter is used to generate the event for a certain amount of time
+    // in the future or past i.e. supply a positive value for the future, negative
+    // value for the past
+    buildEventDate(hoursOffset: number): string[] {
+        
+        let date = new Date();
+        date.setHours(date.getHours() + hoursOffset);
+        let eventDate = date.toISOString().slice(0, 10);
+        let hour = date.getHours();
+        let eventStartTime = hour + ':00';
+        let eventEndTime = (hour + 2) + ':00';
+        console.log('date, start, end', eventDate, eventStartTime, eventEndTime);
+        return [eventDate, eventStartTime, eventEndTime];
+    }
+}
